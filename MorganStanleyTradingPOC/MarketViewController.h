f1ba28ad53b136f963fff7e12ed520e5f24f1fdc
Anukul-Bhatnagar-FS <anukul.bhatnagar@firstgroup.com>
2025-07-04 15:42:37 +0530

    Added New tab market view.

---
+++ MorganStanleyTradingPOC/MarketViewController.h
@@ -0,0 +1,16 @@
+//
+//  MarketViewController.h
+//  MorganStanleyTradingPOC
+//
+//  Created by Anukul Bhatnagar on 04/07/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MarketViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MorganStanleyTradingPOC/MarketViewController.m
@@ -0,0 +1,31 @@
+//
+//  MarketViewController.m
+//  MorganStanleyTradingPOC
+//
+//  Created by Anukul Bhatnagar on 04/07/25.
+//
+
+#import "MarketViewController.h"
+
+@interface MarketViewController ()
+
+@end
+
+@implementation MarketViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
